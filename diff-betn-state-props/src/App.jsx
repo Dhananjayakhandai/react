3474640287d5c481c13afaae9bd3e379e170b7c1
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-09-09 20:24:53 +0530

    conditional operator

--- diff-betn-state-props/src/App.jsx
+++ diff-betn-state-props/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import Greeting from './Greeting'
 
 function App() {
-  console.log('Parent component ')
+  // console.log('Parent component ')
   const [name, setName] = useState('Rahul')
+  const [showGreeting, setShowgreeting] = useState(true)
+  console.log('showGreeting', showGreeting)
 
   return (
     <>
       <div style={{ padding: '20px' }}>
         <h1>React Props vs States</h1>
 
+        <button onClick={setShowgreeting(!showGreeting)}>
+         {showGreeting ? 'Hide': 'show'}
+        </button>
+
         {/* <Greeting name ={name}/> */}
-        <Greeting name={name}>
-          <p>this is an extra element passed as <strong>children</strong></p>
-          we can pass more elements, text, components etc.
-        </Greeting>
+        {
+          showGreeting &&
+          <Greeting name={name}>
+            <p>this is an extra element passed as <strong>children</strong></p>
+            we can pass more elements, text, components etc.
+          </Greeting> 
+        }
+
+
         <button style={{ marginTop: '10px' }} onClick={() => { setName((prev) => prev + 1) }}>click me</button>
       </div>
     </>
