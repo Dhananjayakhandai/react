cf4cd4aa941dfc1ce3b31f1d388e487b3cd2daf1
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-09-13 20:41:10 +0530

    search functionalities

--- rest-countries/src/components/CountriesList.jsx
+++ rest-countries/src/components/CountriesList.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import countriesData from '../countriesData'
 import CountryCard from './CountryCard'
 
-export default function CountriesList() {
-    console.log('.......................', countriesData[0])
+export default function CountriesList({query}) {
+    // const [query, setQuery] = useState('')
+    const filteredCountries = countriesData.filter((country) =>
+                         country.name.common.toLocaleLowerCase().includes(query))
     return (
-        <div className="countries-container">
-            {
-                countriesData.map(
-                    (country, idx) => {
-                        return(
-                        <CountryCard 
-                        key={idx}
-                        flag={country.flags.svg}
-                        name={country.name.common}
-                        population={country.population}
-                        region={country.region}
-                        capital={country.capital[0]}
+        <>
+            <div className="countries-container">
+                {
+                    filteredCountries.map(
+                        (country, idx) => {
+                            return (
+                                <CountryCard
+                                    key={idx}
+                                    flag={country.flags.svg}
+                                    name={country.name.common}
+                                    population={country.population}
+                                    region={country.region}
+                                    capital={country.capital[0]}
 
-                        />
-                        )
-                    }
-                
-                )
-            }
-        </div>
+                                />
+                            )
+                        }
+
+                    )
+                }
+            </div>
+        </>
     )
 }
 
