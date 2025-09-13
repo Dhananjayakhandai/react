f7c5b250f132d486f348429ebba5544e1a5416a5
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-09-13 21:26:23 +0530

    dynamically fetch countries data

--- rest-countries/src/components/CountriesList.jsx
+++ rest-countries/src/components/CountriesList.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
-import countriesData from '../countriesData'
+// import countriesData from '../countriesData'
 import CountryCard from './CountryCard'
 
-export default function CountriesList({query}) {
-    // const [query, setQuery] = useState('')
+export default function CountriesList({ query }) {
+    // let countriesData = []
+    const[countriesData, setcountriesData] = useState([])
+
+    if(countriesData.length === 0){
+        fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital')
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            // console.log('data:', data)
+            // countriesData = data
+            setcountriesData(data)
+        })
+    }
+   
+        console.log('countriesData', countriesData)
+
     const filteredCountries = countriesData.filter((country) =>
-                         country.name.common.toLocaleLowerCase().includes(query))
+        country.name.common.toLocaleLowerCase().includes(query))
+
     return (
         <>
+            <button onClick={() => { countriesData =([])}}>Remove all Countries</button>
             <div className="countries-container">
                 {
-                    filteredCountries.map(
+                    // filteredCountries.map(
+                    countriesData.map(
                         (country, idx) => {
                             return (
                                 <CountryCard
