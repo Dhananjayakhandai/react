ccf5236f6c62289d4dac4a4ae5c2b716dd30f2ee
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-09-14 09:22:37 +0530

    intro to react-routing

--- rest-countries/src/main.jsx
+++ rest-countries/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello React Router</h1>
+  },
+  {
+    path: '/abc',
+    element: <p>This is a random paragraph</p>
+  },
+  {
+    path: '/contact',
+    element: <h1>contact path</h1>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  // <App />
+  // {/* </StrictMode>, */}
+  <RouterProvider router={router} />
 )
